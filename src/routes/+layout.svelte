ced89a38a08e1679c1971000ef7a3a93679c8c66
wpknox <knox.wp@gmail.com>
2023-09-06 19:57:42 -0600

    update about page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,8 @@
 <div class="min-h-full px-4">
 	<nav class="navbar bg-base-100 text-primary-content border-b">
 		<div class="flex-1 gap-2">
-			<a href="/" class="btn btn-ghost normal-case text-xl">Field Finder</a>
-			<a href="/about" class="link link-hover normal-case ml-2 ms-auto mr-4">About</a>
+			<a href="/" class="btn btn-outline btn-primary normal-case text-xl">Field Finder</a>
+			<a href="/about" class="link link-hover link-info normal-case ml-2 ms-auto mr-4">About</a>
 		</div>
 	</nav>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,41 @@
-<div class="flex flex-col items-center w-full h-full">
-	<h1 class="text-7xl font-bold">About Field Finder</h1>
+<div class="flex flex-col w-full h-full">
+	<h1 class="text-7xl font-bold text-center text-info">About Field Finder</h1>
+
+	<div class="p-4 m-4">
+		<p class="text-lg pb-4 font-semibold">
+			Field Finder is a tool that allows you to search for various crop fields near your location.
+			To search simply do the following:
+		</p>
+		<ol class="list-decimal pl-12">
+			<li>Select the types of fields you wish to find</li>
+			<li>Set the search radius</li>
+			<li>Click on the map where you are located/where you wish to center your search from</li>
+			<li>Press the 'Search' button</li>
+		</ol>
+		<p class="text-md pt-8 pb-1 font-medium">
+			Field Finder will then find all fields that meet your criteria and list them for you. If you
+			wish to inspect a field in greater detail, you can click on it to find more information.
+		</p>
+		<p class="text-md py-1 font-medium">
+			Under the hood, I am leveraging the
+			<a
+				href="https://www.ers.usda.gov/developer/data-apis/arms-data-api/"
+				target="_blank"
+				class="link link-hover link-secondary font-bold">USDA ARMS API</a
+			> to gather field/crop data.
+		</p>
+		<p class="text-md py-1 font-medium">
+			This application was initially inspired by my friends and I when we were out dove hunting in
+			an area we were unfamiliar with. It can be hard to determine what areas to explore, especially
+			when you are in a new area. Knowing what crops are around you and where can help a lot in
+			determining whether or not a hunting location is viable for doves and other upland birds.
+		</p>
+		<p class="py-6 font-light">
+			<span class="italic">Note: </span> A commercial product
+			<a href="https://www.onxmaps.com/" class="link link-hover link-secondary font-bold">onX</a>
+			has very similar features/use cases, but it is a paid application/service with a much larger scope
+			than <span class="font-semibold">Field Finder</span>. I am simply trying to make a simple way
+			for someone to determine what types of fields are close to them (for now...).
+		</p>
+	</div>
 </div>
